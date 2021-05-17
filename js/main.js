970a7f136aafa91891ca56b4c36fcb621b469a53
hasanNaim6246 <hasannaim6246@gmail.com>
2021-05-17 21:21:06 +0600

    done some styling and fix some responsive issues

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+// Code for Navigation
+
 const toggleIcon = document.querySelector(".navbar_toggle")
+const nav = document.querySelector(".div_items")
 
 let classToggle = () => {
-    const navs = document.querySelector(".div_items")
-    navs.classList.toggle("nav_show")
+    nav.classList.toggle("nav_show")
 }
 
-toggleIcon.addEventListener("click", classToggle);                                                                                                          
+toggleIcon.addEventListener("click", classToggle);  
+
+
+
+
+// Code for Hide Navigation
+
+const everySectionWithoutHeader = [ document.querySelector("#about"),
+                document.querySelector("#home"),
+                document.querySelector("#project")
+            ];
+
+const navbarLinks = document.querySelectorAll(".navbar_links")
+
+const removeClassFromHeader = () => {
+    nav.classList.remove("nav_show")  
+    }
+
+everySectionWithoutHeader.forEach((section) => {
+    section.addEventListener("click", removeClassFromHeader)
+})
+
+navbarLinks.forEach( element => {
+    element.addEventListener("click", removeClassFromHeader)
+})
